15300b682f21866f283bbaeb47eed79a36578c20
janghoonseob <arpy1228@gmail.com>
2022-04-25 13:48:52 +0900

    feat: interceptor 설정

---
+++ src/main/java/hello/login/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,19 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+
+        return true;
+    }
+}
