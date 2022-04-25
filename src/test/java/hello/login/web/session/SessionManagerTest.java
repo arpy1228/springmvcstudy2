6a0b6cc492f549759500098545f9d72d03427102
janghoonseob <arpy1228@gmail.com>
2022-04-25 09:20:08 +0900

    feat: login session 기능

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,31 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        Object result = sessionManager.getSession(request);
+
+        assertThat(result).isEqualTo(member);
+
+    }
+
+}
